2de4475819bc9de8bf5d953058f87f37f7150308
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-22 23:51:04 +0900

    Create 2739.cpp

---
+++ baekjoon/2739.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for(int i = 1; i < 10; i++)
+    {
+        cout << N << " * " << i << " = " << N*i << endl;
+    }
+
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/2739
