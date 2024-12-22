da77314c1690cf81f538557d9c7f65c08ea812b1
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-22 22:54:32 +0900

    upload baekjoon 10926 task

---
+++ baekjoon/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string case1;
+
+    cin >> case1;
+
+    if(case1.length() <= 50)
+    {
+        cout << case1 << "??!\n" << endl;
+    }
+
+    return 0;
+}
