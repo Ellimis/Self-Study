c90961c98020d6f1528dda2876636cc877fdf80e
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-22 23:08:31 +0900

    Add files via upload

---
+++ baekjoon/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a+b+c << endl;
+
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/11382
