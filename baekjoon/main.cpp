77c39e0b9c8cf5d3b9c28dff2c00c743eca07529
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-22 23:00:20 +0900

    upload baekjoon 18108 task

---
+++ baekjoon/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y;
+
+    cin >> y;
+
+    if(1000 <= y && y <= 3000)
+    {
+        cout << y-543 << "\n" << endl;
+    }
+
+    return 0;
+}
