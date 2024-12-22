33b5e339816cb24fe42692e15580299596b2a625
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-22 23:59:20 +0900

    Create 8393.cpp

---
+++ baekjoon/8393.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, sum = 0;
+    
+    cin >> N;
+
+    for(int i = 1; i <= N; i++)
+    {
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/8393
