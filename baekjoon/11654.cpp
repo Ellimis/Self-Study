901c2549adacc6d8f763690a3603cd91aa533b86
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-26 23:31:54 +0900

    Create 11654.cpp

---
+++ baekjoon/11654.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char C;
+
+    cin >> C;
+
+    cout << static_cast<int>(C) << "\n";
+
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/11654
