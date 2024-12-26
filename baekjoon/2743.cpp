f86bcaae533caffd927737fc454dabe945989a09
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-26 23:09:31 +0900

    Create 2743.cpp

---
+++ baekjoon/2743.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+
+    getline(cin, s);
+    
+    cout << s.length() << "\n";
+    
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/2743
