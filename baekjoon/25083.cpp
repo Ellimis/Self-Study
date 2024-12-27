b5af3a98ec1324d0312307fed336802240937de5
Ellimis <67461878+Ellimis@users.noreply.github.com>
2024-12-27 21:43:02 +0900

    Create 25083.cpp

---
+++ baekjoon/25083.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "         ,r\'\"7" << "\n";
+    cout << "r`-_   ,\'  ,/" << "\n";
+    cout << " \\. \". L_r\'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+
+    return 0;
+}
+
+/* 
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+
+// https://www.acmicpc.net/problem/25083
