3eb0824184e79970b08016ce1fd76c9ca0e93349
Ellimis <67461878+Ellimis@users.noreply.github.com>
2025-01-31 21:47:10 +0900

    Create 27323.cpp

---
+++ baekjoon/27323.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    int A, B;
+
+    cin >> A;
+    cin >> B;
+
+    cout << A*B;
+
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/27323
