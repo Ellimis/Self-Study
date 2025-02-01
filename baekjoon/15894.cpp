970207bc2c5c1b5eb3266ac128ff7271cc3cb3fe
Ellimis <67461878+Ellimis@users.noreply.github.com>
2025-02-01 21:20:42 +0900

    Create 15894.cpp

---
+++ baekjoon/15894.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    long N;
+
+    cin >> N;
+
+    cout << N*4;
+
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/15894
+// int로 써서 한번 틀렸음. 알고리즘 문제에 입력 범위에 대해 잘봐야겠음..
